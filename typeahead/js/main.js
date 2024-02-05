92867163e0ad011b15cf7969feefe1b7fd487640
varun singhal <varunsinghal3655@gmail.com>
2024-02-05 23:56:16 +0530

    predictions are working properly click works change the input value

--- typeahead/js/main.js
+++ typeahead/js/main.js
@@ -13,10 +13,42 @@ const input = typeahead.querySelector('input')
 input.addEventListener('input', evt => {
     const input = evt.target
     const inputValue = input.value.trim().toLowerCase()
+    ul.innerHTML = ''
+    if (!inputValue) return
+    ul.removeAttribute('hidden')
     const matches = countries.filter(country => {
         const name = country.name.toLowerCase()
-        const newListElement = document.createElement('li')
-        // newListElement.innerText = name.startsWith(inputValue)
-        // ul.appendChild(newListElement)
+        return name.startsWith(inputValue)
     })
+
+    const listOfMatchedInputElement = matches.map(createListElement)
+
+    listOfMatchedInputElement.forEach(
+        listElement => {
+            ul.appendChild(listElement)
+        }
+    )
+})
+
+const createListElement = matchedInput => {
+    const listElement = document.createElement('li')
+    listElement.classList.add('matched_suggestions')
+    listElement.innerText = `${matchedInput.name}`
+
+    return listElement
+}
+
+
+document.addEventListener('click', (evt) => {
+    if (!evt.target.closest('.typeahead')) {
+        ul.setAttribute('hidden', true)
+    }
+})
+
+ul.addEventListener('click', (evt) => {
+    if (!evt.target.matches('li')) return
+
+    const listElement = evt.target;
+    input.value = listElement.innerText
+    ul.setAttribute('hidden', true)
 })
