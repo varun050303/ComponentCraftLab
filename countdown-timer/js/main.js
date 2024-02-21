ee2b2d5269866fb065f1f46e78b7bdc3bf9739f7
varunsinghal <varunsinghal3655@>
2024-02-21 13:30:21 +0530

    update

--- countdown-timer/js/main.js
+++ countdown-timer/js/main.js
@@ -1,9 +1,22 @@
-let count = -1;
+const toMilliseconds = unit => {
+    const seconds = 1000
+    const minutes = seconds * 60
+    const hours = minutes * 60
+    const days = hours * 24
 
-const callSuperHeroes = (...superHeroes) => {
-    count = count + 1
-    if (!superHeroes[count]) return clearInterval(id)
-    console.log(`${superHeroes[count]} Sup!!`)
+    if (unit === 'seconds') return seconds
+    if (unit === 'minutes') return minutes
+    if (unit === 'hours') return hours
+    if (unit === 'days') return days
 }
 
-const id = setInterval(callSuperHeroes, 2000, 'Hulk', 'Iron-Man', 'Thor')
+const date1 = new Date(2024, 2, 0, 0, 10, 21)
+const date2 = new Date(2024, 2, 0, 0, 0, 0)
+
+const difference = (date1 - date2)
+const minutes = Math.floor(difference / toMilliseconds('minutes'))
+const remainder = difference - minutes * toMilliseconds('minutes')
+const seconds = remainder / toMilliseconds('seconds')
+
+console.log(minutes)
+console.log(seconds)
