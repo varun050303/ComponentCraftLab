e2f87e1723549102b19d35fcc66ed6d776146b76
varun singhal <varunsinghal3655@gmail.com>
2024-03-21 16:06:52 +0530

    refactor code

--- infinite_scroll/js/main.js
+++ infinite_scroll/js/main.js
@@ -1,54 +1,55 @@
 const endpoint = 'https://api.learnjavascript.today/letters'
 const lettersEl = document.querySelector('.letters')
-const button = document.querySelector('button')
+const loadMoreButton = document.querySelector('button')
 const spinner = document.querySelector('.spinner')
 const dribbbleSection = document.querySelector('.dribbble-section')
 
 
-fetch(`${endpoint}?limit=6&page=1`).then(r => r.json())
-    .then(body => {
-        const { letters, nextPage } = body
-        button.dataset.nextPage = nextPage
-        spinner.setAttribute('hidden', true)
-        const fragment = document.createDocumentFragment()
-        letters.forEach(letter => {
-            const li = document.createElement('li')
-            li.innerHTML =
-                `<a class="letter" href="${letter.shortUrl}">
-                <span>By ${letter.creator} </span>
-                <img src="${letter.imageUrl}" alt="Picture of ${letter.letter}" width="400" height="300">            
-            </a>
-            `
-            fragment.appendChild(li)
-            lettersEl.appendChild(fragment)
-        });
-    })
+const hideElement = element => {
+    element.setAttribute('hidden', true)
+}
 
-button.addEventListener('click', () => {
-    spinner.removeAttribute('hidden')
-    button.setAttribute('hidden', true)
-    fetch(`${button.dataset.nextPage}`).then(r => r.json())
+const showElement = element => {
+    element.removeAttribute('hidden')
+}
+
+const addLettersToDom = (letters) => {
+    const fragment = document.createDocumentFragment()
+    letters.forEach(letter => {
+        const li = document.createElement('li')
+        li.innerHTML =
+            `<a class="letter" href="${letter.shortUrl}">
+            <span>By ${letter.creator} </span>
+            <img src="${letter.imageUrl}" alt="Picture of ${letter.letter}" width="400" height="300">            
+        </a>
+        `
+        fragment.appendChild(li)
+    });
+    lettersEl.appendChild(fragment)
+}
+
+
+const fetchLetters = _ => {
+    showElement(spinner)
+    hideElement(loadMoreButton)
+
+    //fetch letters
+    fetch(loadMoreButton.dataset.nextPage).then(r => r.json())
         .then(body => {
             const { letters, nextPage } = body
-            const fragment = document.createDocumentFragment()
-            letters.forEach(letter => {
-                const li = document.createElement('li')
-                li.innerHTML =
-                    `<a class="letter" href="${letter.shortUrl}">
-                <span>By ${letter.creator} </span>
-                <img src="${letter.imageUrl}" alt="Picture of ${letter.letter}" width="400" height="300">            
-            </a>
-            `
-                fragment.appendChild(li)
-                lettersEl.appendChild(fragment)
-                spinner.setAttribute('hidden', true)
-                button.removeAttribute('hidden')
-                if (nextPage) {
-                    button.dataset.nextPage = nextPage
-                } else {
-                    button.setAttribute('hidden', true)
-                    dribbbleSection.removeAttribute('hidden')
-                }
-            })
+            addLettersToDom(letters)
+            hideElement(spinner)
+            showElement(loadMoreButton)
+
+            if (nextPage) {
+                loadMoreButton.dataset.nextPage = nextPage
+            } else {
+                hideElement(loadMoreButton)
+                showElement(dribbbleSection)
+            }
         })
-})
+}
+
+loadMoreButton.addEventListener('click', fetchLetters)
+loadMoreButton.dataset.nextPage = `${endpoint}?limit=6&page=1`
+loadMoreButton.click()
